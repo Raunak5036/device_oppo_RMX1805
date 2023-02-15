d1a18b37113254854b2486aef9c0e8a7cc4ee289
Raunak5036 <125304874+Raunak5036@users.noreply.github.com>
2023-02-15 20:06:34 +0530

    Lets bulid havoc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_RMX1805.mk
+    $(LOCAL_DIR)/havoc_RMX1805.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_RMX1805-user \
-    lineage_RMX1805-userdebug \
-    lineage_RMX1805-eng
+    havoc_RMX1805-user \
+    havoc_RMX1805-userdebug \
+    havoc_RMX1805-eng
